fea4715afad3efd5d1844bca99b2fc571b19d9d1
Anh Luu <conerser14@gmail.com>
2022-05-18 22:17:32 -0400

    First Change

---
+++ src/app/classes/users.ts
@@ -0,0 +1,15 @@
+export class Users 
+{
+    constructor
+    (
+    public id: number,
+    public name: string,
+    public username: string,
+    public email: string,
+    public address: string,
+    public phone:string,
+    public website:string,
+    public company: string
+    ) {      
+    }
+}
